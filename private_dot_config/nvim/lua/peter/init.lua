cf8744df7ba597439a0cfd376f1320c7f38573a1
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-08-31 17:51:49 +0100

    nvim: run `chezmoi apply` when saving config files

--- private_dot_config/nvim/lua/peter/init.lua
+++ private_dot_config/nvim/lua/peter/init.lua
@@ -1,2 +1,14 @@
 require("peter.set")
 require("peter.packer")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+peter_group = augroup("peter", { clear = true })
+
+autocmd("BufWritePost", {
+    group = peter_group,
+    -- This lua api doesn't expand the home directory by default
+    pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+    command = '!chezmoi apply --source-path "%"',
+})
