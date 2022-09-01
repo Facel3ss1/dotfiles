37d295a2b1b77444b0c679a54b19389159f66dfa
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-09-01 23:19:56 +0100

    nvim: refactor highlight into file

--- private_dot_config/nvim/after/plugin/color.lua
+++ private_dot_config/nvim/after/plugin/color.lua
@@ -1,10 +1,8 @@
+local hl = require("peter.highlight").hl
+
 vim.opt.termguicolors = true
 vim.g.ayucolor = "mirage"
 vim.cmd("colorscheme ayu")
-
-local function hl(group, opts)
-    vim.api.nvim_set_hl(0, group, opts)
-end
 
 -- Remove background
 hl("Normal", {
