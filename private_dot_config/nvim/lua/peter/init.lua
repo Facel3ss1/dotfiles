2405b4b2bd186826a046bbb4642b32c78a54ed59
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-09-02 18:57:26 +0100

    nvim: don't run `chezmoi apply` on files in the .git folder

--- private_dot_config/nvim/lua/peter/init.lua
+++ private_dot_config/nvim/lua/peter/init.lua
@@ -4,11 +4,17 @@ require("peter.packer")
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-peter_group = augroup("peter", { clear = true })
+local chezmoi_dir = vim.fn.expand("~") .. "/.local/share/chezmoi/"
 
 autocmd("BufWritePost", {
-    group = peter_group,
-    -- This lua api doesn't expand the home directory by default
-    pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*",
-    command = '!chezmoi apply --source-path "%"',
+    group = augroup("ChezmoiApplyOnSave", { clear = true }),
+    pattern = chezmoi_dir .. "*",
+    callback = function(opts)
+        -- Ignore paths in the .git folder
+        local path = opts.match
+        local git_paths = vim.fn.glob(chezmoi_dir .. ".git/*", false, true)
+        if vim.fn.index(git_paths, path) < 0 then 
+            vim.cmd('!chezmoi apply --source-path "%"')
+        end
+    end
 })
