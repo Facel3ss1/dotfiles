5e9dbaee60eb4dd0803f1561d967e27917e7279a
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-09-05 23:42:06 +0100

    nvim: Change neogit arrows to arrow icons

--- private_dot_config/nvim/after/plugin/neogit.lua
+++ private_dot_config/nvim/after/plugin/neogit.lua
@@ -3,3 +3,10 @@ local nnoremap = require("peter.keymap").nnoremap
 
 nnoremap("<leader>gs", neogit.open)
 nnoremap("<leader>gc", function() neogit.open {"commit"} end)
+
+neogit.setup {
+    signs = {
+        section = {"", ""},
+        item = {"", ""},
+    },
+}
