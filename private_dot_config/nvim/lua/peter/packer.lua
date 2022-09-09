1730f36f5eff7c3002c8d07a955ef2eb9ca04462
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-09-09 01:37:34 +0100

    nvim: always preview new packer updates
    I don't want to have to suddenly deal with breaking changes if I can
    avoid it.

--- private_dot_config/nvim/lua/peter/packer.lua
+++ private_dot_config/nvim/lua/peter/packer.lua
@@ -5,54 +5,59 @@ if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
 end
 
-return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
+return require("packer").startup {
+    function(use)
+        use "wbthomason/packer.nvim"
 
-    use "Shatur/neovim-ayu"
+        use "Shatur/neovim-ayu"
 
-    -- use "tpope/vim-sleuth"
+        -- use "tpope/vim-sleuth"
 
-    use "numToStr/Comment.nvim"
-    use {
-        "kylechui/nvim-surround",
-        tag = "*",
-        config = function() require("nvim-surround").setup {} end
+        use "numToStr/Comment.nvim"
+        use {
+            "kylechui/nvim-surround",
+            tag = "*",
+            config = function() require("nvim-surround").setup {} end
+        }
+        use {
+            "windwp/nvim-autopairs",
+            config = function() require("nvim-autopairs").setup {} end
+        }
+
+        use "lukas-reineke/indent-blankline.nvim"
+
+        use "lewis6991/gitsigns.nvim"
+        use {"TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim"}
+
+        use {"kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons"}
+        use {"nvim-lualine/lualine.nvim", requires = "kyazdani42/nvim-web-devicons"}
+        use "folke/which-key.nvim"
+
+        use {
+            "nvim-telescope/telescope.nvim",
+            branch = "0.1.x",
+            requires = "nvim-lua/plenary.nvim",
+        }
+        use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
+
+        use "hrsh7th/nvim-cmp"
+        use "hrsh7th/cmp-cmdline"
+        use "hrsh7th/cmp-buffer"
+        use "hrsh7th/cmp-path"
+        use "hrsh7th/cmp-nvim-lua"
+
+        use {"L3MON4D3/LuaSnip", tag = "v1.*"}
+        use "saadparwaiz1/cmp_luasnip"
+
+        use "onsails/lspkind.nvim"
+
+        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+
+        if packer_bootstrap then
+            require("packer").sync()
+        end
+    end,
+    config = {
+        preview_updates = true,
     }
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
-
-    use "lukas-reineke/indent-blankline.nvim"
-
-    use "lewis6991/gitsigns.nvim"
-    use {"TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim"}
-
-    use {"kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons"}
-    use {"nvim-lualine/lualine.nvim", requires = "kyazdani42/nvim-web-devicons"}
-    use "folke/which-key.nvim"
-
-    use {
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        requires = "nvim-lua/plenary.nvim",
-    }
-    use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
-
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-cmdline"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-nvim-lua"
-
-    use {"L3MON4D3/LuaSnip", tag = "v1.*"}
-    use "saadparwaiz1/cmp_luasnip"
-
-    use "onsails/lspkind.nvim"
-
-    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-
-    if packer_bootstrap then
-        require("packer").sync()
-    end
-end)
+}
