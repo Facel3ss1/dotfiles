372a7de5ab5d9df96871807a48c5907116862968
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-10-03 23:09:30 +0100

    nvim: Disable treesitter highlighting for help files

--- private_dot_config/nvim/lua/peter/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/peter/plugins/treesitter.lua
@@ -39,6 +39,7 @@ require("nvim-treesitter.configs").setup {
     auto_install = true,
     highlight = {
         enable = true,
+        disable = {"help"},
         additional_vim_regex_highlighting = false,
     },
     endwise = {
