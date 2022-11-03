9bf142d24f9b4c1570bee6cdfff5a43ae63b729a
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2022-11-03 20:29:54 +0000

    nvim: Format options.lua

--- private_dot_config/nvim/lua/peter/config/options.lua
+++ private_dot_config/nvim/lua/peter/config/options.lua
@@ -44,7 +44,8 @@ vim.o.splitbelow = true
 -- Use powershell on Windows (see :h shell-powershell)
 if vim.fn.has("win32") == 1 then
     vim.o.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
-    vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.o.shellcmdflag =
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
     vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
     vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
     vim.o.shellquote = ""
