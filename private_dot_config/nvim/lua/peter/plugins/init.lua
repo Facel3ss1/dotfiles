f23c864a7200053d309ff09158ab9046bd5d694f
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-02-03 23:16:40 +0000

    nvim: Simplify crates.nvim config

--- private_dot_config/nvim/lua/peter/plugins/init.lua
+++ private_dot_config/nvim/lua/peter/plugins/init.lua
@@ -3,35 +3,17 @@ return {
     { "dstein64/vim-startuptime", cmd = "StartupTime" },
     {
         "saecki/crates.nvim",
-        version = "0.4.0",
-        event = "BufRead Cargo.toml",
-        config = function(_, opts)
-            local crates = require("crates")
+        version = "*",
+        event = { "BufRead Cargo.toml" },
+        opts = {
+            on_attach = function(bufnr)
+                require("cmp").setup.buffer { sources = { { name = "crates" } } }
 
-            vim.api.nvim_create_autocmd("BufRead", {
-                group = vim.api.nvim_create_augroup("CargoCrates", { clear = true }),
-                pattern = "Cargo.toml",
-                callback = function(args)
-                    require("cmp").setup.buffer { sources = { { name = "crates" } } }
-
-                    require("which-key").register({ c = { name = "crates" } }, { prefix = "<leader>c" })
-
-                    local map = function(lhs, rhs, map_opts)
-                        map_opts.buffer = args.buf
-                        vim.keymap.set("n", lhs, rhs, map_opts)
-                    end
-                    -- TODO: Change this mapping to K?
-                    map("<leader>ccp", crates.show_crate_popup, { desc = "Show crate popup" })
-                    map("<leader>ccv", crates.show_versions_popup, { desc = "Show crate versions" })
-                    map("<leader>ccf", crates.show_features_popup, { desc = "Show crate features" })
-                    map("<leader>ccd", crates.show_dependencies_popup, { desc = "Show crate dependencies" })
-                end,
-                desc = "Register crates.nvim keybindings",
-            })
-
-            crates.setup(opts)
-        end,
-        dependencies = "nvim-lua/plenary.nvim",
+                vim.keymap.set("n", "<leader>cC", function()
+                    require("crates").show_popup()
+                end, { desc = "Show crate popup", buffer = bufnr })
+            end,
+        },
     },
     -- TODO: typst.nvim or treesitter parser? They were both WIP when I added this
     {
