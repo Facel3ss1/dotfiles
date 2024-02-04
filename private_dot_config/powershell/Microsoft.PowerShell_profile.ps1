51d3621bb0e52fc609d9f60aafa533df87c31cbb
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-02-04 01:01:12 +0000

    pwsh: Remove fnm use env

--- private_dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ private_dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,6 @@
 # TODO: Add ls aliases (using eza)
 New-Alias which Get-Command
 
-if (Get-Command fnm -ErrorAction SilentlyContinue) {
-    fnm env --use-on-cd | Out-String | Invoke-Expression
-}
-
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     Invoke-Expression (&starship init powershell)
 }
