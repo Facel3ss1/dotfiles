61ed378677137371c1709625da7da80185a6bef7
Viktor Szépe <viktor@szepe.net>
2024-03-03 20:18:03 +0000

    EditorConfig improvements

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -33,7 +33,7 @@ set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
 # pnpm
 set -gx PNPM_HOME $HOME/.local/share/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 
 if status is-interactive
