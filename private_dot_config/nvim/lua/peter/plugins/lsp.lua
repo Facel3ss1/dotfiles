e5b5d595541c95a767a54ea1ee3de78e16577e9c
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-08-13 21:14:05 +0100

    nvim: Add Lua type annotations for wezterm config

--- private_dot_config/nvim/lua/peter/plugins/lsp.lua
+++ private_dot_config/nvim/lua/peter/plugins/lsp.lua
@@ -391,10 +391,12 @@ return {
         opts = {
             library = {
                 { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
             },
         },
     },
     { "Bilal2453/luvit-meta" },
+    { "justinsgithub/wezterm-types" },
     {
         "mrcjkb/rustaceanvim",
         version = "*",
