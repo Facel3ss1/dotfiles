dc9391ab8a950c727189109bf90bf77af14eac8c
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-10-03 15:15:34 +0100

    nvim: Add venv-selector.nvim plugin

--- private_dot_config/nvim/lua/peter/plugins/init.lua
+++ private_dot_config/nvim/lua/peter/plugins/init.lua
@@ -16,6 +16,29 @@ return {
             },
         },
     },
+    {
+        "linux-cultist/venv-selector.nvim",
+        branch = "regexp",
+        ft = "python",
+        cmd = { "VenvSelect" },
+        keys = {
+            { "<leader>cv", "<Cmd>VenvSelect<CR>", desc = "Select virtualenv", ft = "python" },
+        },
+        opts = {
+            settings = {
+                options = {
+                    notify_user_on_venv_activation = true,
+                },
+            },
+        },
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "nvim-telescope/telescope.nvim",
+            -- TODO
+            -- "mfussenegger/nvim-dap",
+            -- "mfussenegger/nvim-dap-python",
+        },
+    },
     -- TODO: typst.nvim or treesitter parser? They were both WIP when I added this
     {
         "kaarmu/typst.vim",
