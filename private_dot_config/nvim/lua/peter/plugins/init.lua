fcbaf943c9a3d889f876963f14b3584dca323415
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2024-10-28 16:37:48 +0000

    nvim: Add typst-preview.nvim plugin

--- private_dot_config/nvim/lua/peter/plugins/init.lua
+++ private_dot_config/nvim/lua/peter/plugins/init.lua
@@ -39,4 +39,30 @@ return {
             -- "mfussenegger/nvim-dap-python",
         },
     },
+    {
+        "chomosuke/typst-preview.nvim",
+        version = "*",
+        ft = "typst",
+        keys = {
+            { "<leader>cp", "<Cmd>TypstPreview<CR>", desc = "Open typst preview", ft = "typst" },
+        },
+        opts = function()
+            -- We want to use the mason tinymist binary
+            local tinymist_package = require("mason-registry").get_package("tinymist")
+            local tinymist_path = vim.fs.normalize(tinymist_package:get_install_path())
+                .. "/"
+                .. require("typst-preview.fetch").get_tinymist_bin_name()
+
+            return {
+                dependencies_bin = {
+                    ["tinymist"] = tinymist_path,
+                },
+            }
+        end,
+        config = true,
+        build = function()
+            -- This will install the websocat binary, but not tinymist, since we specified the tinymist mason binary above
+            require("typst-preview").update()
+        end,
+    },
 }
