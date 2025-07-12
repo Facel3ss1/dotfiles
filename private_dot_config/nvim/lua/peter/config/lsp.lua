6f55cebf4b283b9c98239d611c83fedc8080feeb
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2025-07-12 13:48:19 +0100

    nvim: Migrate LSP config to Neovim 0.11's `vim.lsp.config`

--- private_dot_config/nvim/lua/peter/config/lsp.lua
+++ private_dot_config/nvim/lua/peter/config/lsp.lua
@@ -1,7 +1,9 @@
+local util = require("peter.util")
+
 -- Enable inlay hints by default
 vim.lsp.inlay_hint.enable()
 
--- Buffer-local LSP config
+-- Buffer-local LSP settings
 local lsp_attach_group = vim.api.nvim_create_augroup("PeterLspAttach", { clear = true })
 vim.api.nvim_create_autocmd("LspAttach", {
     group = lsp_attach_group,
@@ -47,5 +49,95 @@ vim.api.nvim_create_autocmd("LspAttach", {
             vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
         end
     end,
-    desc = "Apply buffer-local LSP config",
+    desc = "Apply buffer-local LSP settings",
 })
+
+-- See `:h lsp-quickstart`, these configs are intended to build upon the configs in https://github.com/neovim/nvim-lspconfig
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = "Replace",
+                postfix = ".",
+            },
+            diagnostics = {
+                libraryFiles = "Disable",
+                unusedLocalExclude = { "_*" },
+                groupSeverity = {
+                    ["unused"] = "Warning",
+                },
+            },
+            format = {
+                enable = false,
+            },
+            hint = {
+                enable = true,
+                arrayIndex = "Disable",
+                setType = true,
+            },
+            workspace = {
+                checkThirdParty = false,
+            },
+        },
+    },
+})
+
+-- Allow toggling typos-lsp diagnostics with keybind
+vim.keymap.set("n", "<leader>ts", function()
+    local clients = vim.lsp.get_clients { name = "typos_lsp" }
+    if #clients ~= 1 then
+        util.error("typos-lsp is not active", { name = "typos" })
+        return
+    end
+
+    local typos_client = clients[1]
+    local typos_ns = vim.lsp.diagnostic.get_namespace(typos_client.id)
+    local diagnostics_enabled = vim.diagnostic.is_enabled { ns_id = typos_ns }
+    diagnostics_enabled = not diagnostics_enabled
+
+    if diagnostics_enabled then
+        util.info("Enabled spell checking", { title = "typos" })
+    else
+        util.info("Disabled spell checking", { title = "typos" })
+    end
+
+    vim.diagnostic.enable(diagnostics_enabled, { ns_id = typos_ns })
+end, { desc = "Toggle typos spell checking" })
+
+vim.lsp.config("typos_lsp", {
+    -- typos-lsp uses `init_options` instead of `settings` for configuration
+    init_options = {
+        diagnosticSeverity = "Hint",
+    },
+})
+
+-- The "rust-analyzer" name is used by rustaceanvim, which is what we want, whereas the "rust_analyzer" name is used by nvim-lspconfig
+vim.lsp.config("rust-analyzer", {
+    settings = {
+        ["rust-analyzer"] = {
+            check = {
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+            },
+        },
+    },
+})
+
+vim.lsp.config("basedpyright", {
+    settings = {
+        basedpyright = {
+            -- We are using Ruff's import organizer instead
+            disableOrganizeImports = true,
+        },
+    },
+})
+
+-- rust-analyzer is enabled by the rustaceanvim plugin
+
+-- tsserver is configured and enabled by the typescript-tools plugin
+-- Note that tsserver is included already within a typescript installation and is not the same thing as typescript-language-server (AKA ts_ls)
+
+-- LSPs can also be configured and enabled using project-local configuration (See `:h 'exrc'`)
+
+vim.lsp.enable { "lua_ls", "typos_lsp", "basedpyright", "ruff" }
--- private_dot_config/nvim/lua/peter/plugins/lsp.lua
+++ private_dot_config/nvim/lua/peter/plugins/lsp.lua
@@ -1,113 +1,23 @@
 local util = require("peter.util")
 local icons = require("peter.util.icons")
 
--- TODO: Use vim.g variables for these
+-- TODO: Use vim.g variable for this
 local enable_format_on_save = true
-local enable_typos_lsp_diagnostics = true
 
 ---@module "lazy"
 ---@type LazySpec
 return {
     {
-        -- FIXME: Migrate to vim.lsp.config in 0.11
         "neovim/nvim-lspconfig",
         version = "*",
-        -- FIXME: Make it work when I :e myfile
         event = "BufReadPre",
-        cmd = { "LspInfo", "LspLog" },
-        keys = {
-            {
-                "<leader>ts",
-                function()
-                    enable_typos_lsp_diagnostics = not enable_typos_lsp_diagnostics
-
-                    if enable_typos_lsp_diagnostics then
-                        util.info("Enabled spell checking", { title = "typos" })
-                    else
-                        util.info("Disabled spell checking", { title = "typos" })
-                    end
-
-                    local clients = vim.lsp.get_clients { name = "typos_lsp" }
-                    if #clients ~= 1 then
-                        return
-                    end
-
-                    local typos_client = clients[1]
-                    local typos_ns = vim.lsp.diagnostic.get_namespace(typos_client.id)
-
-                    vim.diagnostic.enable(enable_typos_lsp_diagnostics, { ns_id = typos_ns })
-                end,
-                desc = "Toggle typos spell checking",
-                mode = { "n" },
-            },
-        },
+        cmd = { "LspRestart", "LspLog" },
         dependencies = { "hrsh7th/cmp-nvim-lsp" },
         config = function()
-            local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            lspconfig.lua_ls.setup {
+            vim.lsp.config("*", {
                 capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        completion = {
-                            callSnippet = "Replace",
-                            postfix = ".",
-                        },
-                        diagnostics = {
-                            libraryFiles = "Disable",
-                            unusedLocalExclude = { "_*" },
-                            groupSeverity = {
-                                ["unused"] = "Warning",
-                            },
-                        },
-                        format = {
-                            enable = false,
-                        },
-                        hint = {
-                            enable = true,
-                            arrayIndex = "Disable",
-                            setType = true,
-                        },
-                        workspace = {
-                            checkThirdParty = false,
-                        },
-                    },
-                },
-            }
-
-            lspconfig.typos_lsp.setup {
-                capabilities = capabilities,
-                -- typos-lsp uses `init_options` instead of `settings` for configuration
-                init_options = {
-                    diagnosticSeverity = "Hint",
-                },
-                -- Set initial state of typos-lsp diagnostics
-                on_attach = function(client, _buf)
-                    -- TODO: Make the typos_lsp code actions do an LSP rename?
-                    local typos_ns = vim.lsp.diagnostic.get_namespace(client.id)
-                    vim.diagnostic.enable(enable_typos_lsp_diagnostics, { ns_id = typos_ns })
-                end,
-            }
-
-            lspconfig.basedpyright.setup {
-                capabilities = capabilities,
-                settings = {
-                    basedpyright = {
-                        -- We are using Ruff's import organizer instead
-                        disableOrganizeImports = true,
-                    },
-                },
-            }
-
-            lspconfig.ruff.setup {
-                capabilities = capabilities,
-            }
-
-            -- rust_analyzer is configured using the rustaceanvim plugin below
-
-            -- tsserver is configured using the typescript-tools plugin below
-            -- Note that tsserver is included already within a typescript installation and is not the same thing as typescript-language-server
+            })
         end,
     },
     {
@@ -217,21 +127,6 @@ return {
         "mrcjkb/rustaceanvim",
         version = "*",
         lazy = false, -- This plugin is already lazy
-        opts = {
-            server = {
-                settings = {
-                    ["rust-analyzer"] = {
-                        check = {
-                            command = "clippy",
-                            extraArgs = { "--no-deps" },
-                        },
-                    },
-                },
-            },
-        },
-        config = function(_, opts)
-            vim.g.rustaceanvim = vim.tbl_deep_extend("force", {}, opts or {})
-        end,
     },
     {
         "pmizio/typescript-tools.nvim",
@@ -244,7 +139,7 @@ return {
                 on_attach = function(_client, buf)
                     vim.keymap.set(
                         "n",
-                        "gs",
+                        "grs",
                         "<Cmd>TSToolsGoToSourceDefinition<CR>",
                         { buffer = buf, desc = "Go to source definition" }
                     )
